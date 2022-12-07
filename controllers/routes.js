355699e353612619ad7665a8799764e757a4a74e
HericMendez <heric.mendez00@gmail.com>
2022-12-07 13:11:56 -0300

    first commit

---
+++ controllers/routes.js
@@ -0,0 +1,17 @@
+
+
+const menu = require('../models/db.json')
+
+
+
+module.exports = (app) =>{
+
+
+    app.get("/cardapio", async (req, res)=>{
+        const cardapio = await menu;
+        console.log(cardapio)
+        res.status(200).json(cardapio);
+    })
+
+
+}
