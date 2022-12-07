75191fb25a24531f1a9bbd1554bd68785da40998
HericMendez <heric.mendez00@gmail.com>
2022-12-07 13:20:06 -0300

    route change

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,7 +7,7 @@ const menu = require('../models/db.json')
 module.exports = (app) =>{
 
 
-    app.get("/cardapio", async (req, res)=>{
+    app.get("/", async (req, res)=>{
         const cardapio = await menu;
         console.log(cardapio)
         res.status(200).json(cardapio);
